c024da5e50970fe964afc7ae8b904a8d99074710
dhsimpson <qeadzc9503@naver.com>
2021-03-07 00:05:08 +0900

    feat : FAQ 스타일 적용

--- src/components/sections/content/FAQ.js
+++ src/components/sections/content/FAQ.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components';
-const RightNav = styled.nav`
-border: 1px solid red;
-`;
+import {FAQList} from '../../../config/FAQConfig.js';
+import '../../../css/section/faq.css';
 
 function FAQ() {
   document.body.style.backgroundImage = "url('./background.jpg')";
   return (
-    <>
-      <>FAQ</>
-      {/* FAQ 리스트들 */}
-    </>
+    <section className="section-faq">
+      <div>
+      {
+        FAQList.map(d=>{
+          return (
+            <>
+            <hr/>
+            {d.question}
+            {d.answer}
+            </>
+          )
+        })
+      }
+      </div>
+    </section>
   );
 }
 
