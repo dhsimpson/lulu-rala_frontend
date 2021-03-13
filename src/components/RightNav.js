a59934a1762f5677da33a1981fb831c6a93a440d
dhsimpson <qeadzc9503@naver.com>
2021-03-13 17:21:36 +0900

    refactor : 우측 네비게이션 바 흰배경 제거 및 3개로 줄임

--- src/components/RightNav.js
+++ src/components/RightNav.js
@@ -3,10 +3,9 @@ function RightNav() {
     return (
       <nav className="nav-right">
         {/* 전부 아이콘으로 대체해야 할 듯함 */}
-        <a href="#"><p>1:1 상담</p></a>
-        <a href="#"><p>모바일체험</p></a>
+        <a href="#"><p>상담 신청</p></a>
+        <a href="#"><p>FAQ</p></a>
         <a href="#"><p>가맹안내</p></a>
-        <a href="#"><p>A/S 신청</p></a>
       </nav>
       );
 }
