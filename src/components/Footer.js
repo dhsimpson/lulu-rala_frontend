7d22c7844a9f31b6262f7663df2d40214591d956
dhsimpson <qeadzc9503@naver.com>
2021-03-14 22:38:35 +0900

    feat : footer 이미지 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,22 @@ import '../css/footer.css';
 function Footer() {
     return (
         <footer>
-        아이들이엔씨<br />
-        사업자번호 : 558-99-00269 ㅣ 대표자 : 이동찬 ㅣ 이메일 : idlenc@naver.com<br />
-        전화 : 02-6052-1021 (상담가능시간 : 10시 ~ 6시) 주말, 공휴일 휴무<br />
-        주소 : 서울시 동대문구 전농로 10길 20, 104호<br />
+            <div className="div-footer-left">
+                <img src="./footer/idlenc.png"/>
+                <div>
+                    <img src="./footer/blog.png"/>
+                    <img src="./footer/cafe.png"/>
+                    <img src="./footer/youtube.png"/>
+                </div>
+            </div>
+            <div className="div-footer-right">
+                <h2>아이들이엔씨</h2>
+                <p>
+                    사업자번호 : 558-99-00269 ㅣ 대표자 : 이동찬 ㅣ 이메일 : idlenc@naver.com<br />
+                    전화 : 02-6052-1021 (상담가능시간 : 10시 ~ 6시) 주말, 공휴일 휴무<br />
+                    주소 : 서울시 동대문구 전농로 10길 20, 104호<br />
+                </p>
+            </div>
         </footer>
     );
 }
