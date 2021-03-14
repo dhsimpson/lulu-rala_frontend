b4ae2e4b0f15a9b5d5f940c7016b01a5a4b55b7c
dhsimpson <qeadzc9503@naver.com>
2021-03-14 22:12:37 +0900

    refactor : 상담신청 css 요구사항에 맞춰 변경 및 개인정보 수집 및 이용 추가

--- src/components/sections/content/RequestForConsultation.js
+++ src/components/sections/content/RequestForConsultation.js
@@ -9,34 +9,57 @@ function RequestForConsultation() {
     <LeftNav menu={navConfig.nav[3]}></LeftNav>
     <section className="section-requestForConsultation">
       <h1>상담 신청</h1>
+      <img src="./section/requestForConsultation.jpg" />
       <form>
-        <h2>LEARNING&amp;CO</h2>
-        <h3>Enter Your Name</h3>
-        <input type="text" placeholder="Name"></input>
-        <h3>Enter Your Email</h3>
-        <input type="text" placeholder="Email"></input>
-        <h3>Enter Your Subject</h3>
-        <input type="text" placeholder="Subject"></input>
-        <h3>Enter Your Message here</h3>
-        <textarea placeholder="Message"></textarea>
-        <br/>
-        <input type="submit" value="Send"></input>
+        <h2>상담신청하기</h2>
+        <div>
+          <div className="input-title"><p>신청자명</p></div>
+          <input type="text"/>
+        </div>
+        <div>
+          <div className="input-title"><p>교습대상자 성명</p></div>
+          <input type="text"/>
+        </div>
+        <div>
+          <div className="input-title"><p>연령(교습대상자)</p></div>
+          <input type="text"/>
+        </div>
+
+        <div>
+          <div className="input-title"><p>연락처</p></div>
+          <input className="input-phone-number" type="text"/>
+          <div className="input-dash"><p>-</p></div>
+          <input className="input-phone-number" type="text"/>
+          <div className="input-dash"><p>-</p></div>
+          <input className="input-phone-number" type="text"/>
+        </div>
+
+        <div className="address">
+          <div className="input-title"><p>주소</p></div>
+          <div className="input-address">
+            <div>
+              <input type="text"/> 
+              <input type="button" value="우편번호 찾기"/>
+            </div>
+            <input type="text"/>
+            <input type="text"/>
+          </div>
+        </div>
+
+        <div>
+          <div className="input-title"><p>희망프로그램</p></div>
+          <input type="text"/>
+        </div>
+        
+        <div className="privacy-statement">
+          개인정보 취급동의
+          <div className="privacy-statemen-checkobx">
+            <input type="checkbox"/>
+            <p>개인정보 수집 및 이용에 동의합니다</p>
+          </div>
+        </div>
+        <input type="submit" value="제출하기"/>
       </form>
-      <div>
-        <p>신청자명<br />
-        교습대상자 성명<br />
-        교습대상자 연령(개월수)<br />
-        연락처<br />
-        주소<br />
-        <br />
-        상담내용<br />
-        <br />
-        업 희망 시간(요일/시간까지 입력)<br />
-        희망 프로그램<br />
-        하단에 개인정보취급방침 동의(체크, 스크롤바)<br />
-        <br />
-        </p>
-      </div>
     </section>
     </>
   );
