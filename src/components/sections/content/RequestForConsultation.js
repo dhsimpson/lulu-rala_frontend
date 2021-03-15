1982d7b5fc230b30eb23cc1e2d09a763c91ec591
dhsimpson <qeadzc9503@naver.com>
2021-03-15 23:32:36 +0900

    feat : 개인정보처리 동의 추가

--- src/components/sections/content/RequestForConsultation.js
+++ src/components/sections/content/RequestForConsultation.js
@@ -1,66 +1,70 @@
 import '../../../css/section/requestForConsultation.css';
 import LeftNav from '../../LeftNav';
 import navConfig from '../../../config/navConfig.json';
+import {useEffect} from 'react';
 
 function RequestForConsultation() {
   document.body.style.backgroundImage = "url('./background.jpg')";
+  useEffect(()=>{
+    document.getElementById("privacyHtml").innerHTML = '<object style="width: 100%; height: 100%" type="text/html" data="privacy.html"/>';
+  })
   return (
     <>
-    <LeftNav menu={navConfig.nav[3]}></LeftNav>
-    <section className="section-requestForConsultation">
-      <h1>상담 신청</h1>
-      <img src="./section/requestForConsultation.jpg" />
-      <form>
-        <h2>상담신청하기</h2>
-        <div>
-          <div className="input-title"><p>신청자명</p></div>
-          <input type="text"/>
-        </div>
-        <div>
-          <div className="input-title"><p>교습대상자 성명</p></div>
-          <input type="text"/>
-        </div>
-        <div>
-          <div className="input-title"><p>연령(교습대상자)</p></div>
-          <input type="text"/>
-        </div>
-
-        <div>
-          <div className="input-title"><p>연락처</p></div>
-          <input className="input-phone-number" type="text"/>
-          <div className="input-dash"><p>-</p></div>
-          <input className="input-phone-number" type="text"/>
-          <div className="input-dash"><p>-</p></div>
-          <input className="input-phone-number" type="text"/>
-        </div>
-
-        <div className="address">
-          <div className="input-title"><p>주소</p></div>
-          <div className="input-address">
-            <div>
-              <input type="text"/> 
-              <input type="button" value="우편번호 찾기"/>
-            </div>
-            <input type="text"/>
-            <input type="text"/>
+      <LeftNav menu={navConfig.nav[3]}></LeftNav>
+      <section className="section-requestForConsultation">
+        <h1>상담 신청</h1>
+        <img src="./section/requestForConsultation.jpg" />
+        <form>
+          <h2>상담신청하기</h2>
+          <div>
+            <div className="input-title"><p>신청자명</p></div>
+            <input type="text" />
           </div>
-        </div>
+          <div>
+            <div className="input-title"><p>교습대상자 성명</p></div>
+            <input type="text" />
+          </div>
+          <div>
+            <div className="input-title"><p>연령(교습대상자)</p></div>
+            <input type="text" />
+          </div>
 
-        <div>
-          <div className="input-title"><p>희망프로그램</p></div>
-          <input type="text"/>
-        </div>
-        
-        <div className="privacy-statement">
-          개인정보 취급동의
+          <div>
+            <div className="input-title"><p>연락처</p></div>
+            <input className="input-phone-number" type="text" />
+            <div className="input-dash"><p>-</p></div>
+            <input className="input-phone-number" type="text" />
+            <div className="input-dash"><p>-</p></div>
+            <input className="input-phone-number" type="text" />
+          </div>
+
+          <div className="address">
+            <div className="input-title"><p>주소</p></div>
+            <div className="input-address">
+              <div>
+                <input type="text" />
+                <input type="button" value="우편번호 찾기" />
+              </div>
+              <input type="text" />
+              <input type="text" />
+            </div>
+          </div>
+
+          <div>
+            <div className="input-title"><p>희망프로그램</p></div>
+            <input type="text" />
+          </div>
+
+          <div className="privacy-statement">
+            <div id="privacyHtml"></div>
+          </div>
           <div className="privacy-statemen-checkobx">
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <p>개인정보 수집 및 이용에 동의합니다</p>
           </div>
-        </div>
-        <input type="submit" value="제출하기"/>
-      </form>
-    </section>
+          <input type="submit" value="제출하기" />
+        </form>
+      </section>
     </>
   );
 }
