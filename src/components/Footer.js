e4ec699b4907e9d3f32c53ef7be8f80f18044020
dhsimpson <qeadzc9503@naver.com>
2021-04-07 01:48:22 +0900

    main, footer 모바일 버전

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,9 @@ function Footer() {
                 </div>
             </div>
             <div className="div-footer-right">
-                <h2>아이들이엔씨</h2>
+                <h2>아이들이엔씨</h2><br/>
                 <p>
-                    사업자번호 : 558-99-00269 ㅣ 대표자 : 이동찬 ㅣ 이메일 : idlenc@naver.com<br />
+                    사업자번호 : 558-99-00269 ㅣ 대표자 : 이동찬 {window.innerWidth > 800 ? <>ㅣ</>:<br/>} 이메일 : idlenc@naver.com<br />
                     전화 : 02-6052-1021 (상담가능시간 : 10시 ~ 6시) 주말, 공휴일 휴무<br />
                     주소 : 서울시 동대문구 전농로 10길 20, 104호<br />
                 </p>
--- src/components/sections/Main.js
+++ src/components/sections/Main.js
@@ -48,7 +48,7 @@ const controlTransition = () => {
   })
 }
 function Main() {
-  document.body.style.backgroundImage = "url('./background-main.jpg')";
+  // document.body.style.backgroundImage = "url('./background-main.jpg')";
   useEffect(() => {
     controlTransition();
     document.getElementById("privacyHtml").innerHTML = `
@@ -76,34 +76,57 @@ ex)
 
   return (
     <>
-      <ReactFullpage
-        // anchors={anchors}
-        navigation={true}
-        normalScrollElements="#consultContent, #privacyHtml"
-        // navigationTooltips={anchors}
-        render={() => {
-          return (
-            <div>
-              <div className="section article-first">
-                {ArticleFirst()}
-              </div>
+      {window.innerWidth >= 800
+        ? <ReactFullpage
+          // anchors={anchors}
+          navigation={true}
+          normalScrollElements="#consultContent, #privacyHtml"
+          // navigationTooltips={anchors}
+          render={() => {
+            return (
+              <div>
+                <div className="section article-first">
+                  {ArticleFirst()}
+                </div>
 
-              <div className="section article-second">동영상 4~5개</div>
+                <div className="section article-second">동영상 4~5개</div>
 
-              <div className="section article-third">
-                {ArticleThird()}
+                <div className="section article-third">
+                  {ArticleThird()}
+                </div>
+                <div className="section article-fourth">
+                  {ArticleFourth()}
+                </div>
+                <div className="section article-fifth">
+                  {ArticleFifth()}
+                </div>
+                <div className="section fp-auto-height"> <Footer /></div>
               </div>
-              <div className="section article-fourth">
-                {ArticleFourth()}
-              </div>
-              <div className="section article-fifth">
-                {ArticleFifth()}
-              </div>
-              <div className="section fp-auto-height"> <Footer /></div>
+            );
+          }}
+        />
+        : <div>
+            <div className="section article-first">
+              {ArticleFirst()}
             </div>
-          );
-        }}
-      />
+
+            <div className="section article-second">동영상 4~5개</div>
+
+            <div className="section article-third">
+              {ArticleThird()}
+            </div>
+            <div className="section article-fourth">
+              {ArticleFourth()}
+            </div>
+            <div className="section article-fifth">
+              {ArticleFifth()}
+            </div>
+            <div className="section fp-auto-height"></div>
+          </div>
+
+      }
+
+
     </>)
 }
 function ArticleFirst() {
@@ -122,24 +145,12 @@ function ArticleFirst() {
 function ArticleThird() {
   return (
     <>
+    <img id="third-img-id" className="third-left" src="./section/luluRalaPlayingEnglish.jpg" alt="아이들이엔씨" />
       <h1 id="third-h1-id" className="MapoPeacefull">Lulu Rala English</h1>
-      <img id="third-img-id" className="third-left" src="./section/luluRalaPlayingEnglish.jpg" alt="아이들이엔씨"/>
       <div id="third-div-id" className="third-right">
-        <p>룰루랄라 노는영어는  영어를 처음 시작하는</p>
-        <p>아이부터 초등학생을 대상으로</p>
-        <p>흥미로운 주제와 다양한 활동이 주가 되어</p>
-        <p>아이들이 영어를 쉽고 재미있게 배울 수 있는</p>
-        <p>영어학습 프로그램입니다.</p><br />
+        <p>룰루랄라 노는영어는  영어를 처음 시작하는 아이부터 초등학생을 대상으로 흥미로운 주제와 다양한 활동이 주가 되어 아이들이 영어를 쉽고 재미있게 배울 수 있는 영어학습 프로그램입니다.</p><br />
 
-        <p>균형적 언어접근법</p>
-        <p>(<span className="MapoPeacefull">Balanced Language Approach</span>)을 기반으로</p>
-        <p>듣고, 말하고, 읽고, 쓰기를 분리하지 않고</p>
-        <p>이야기와 놀이 그리고 다양한 활동으로</p>
-        <p>의사소통능력을 골고루 성장시키며 영어에 대한</p>
-        <p>자신감,</p>
-        <p>영어에 대한 호기심과 욕구를 충족시키고</p>
-        <p>성취감을 느끼도록 하며 영어를 즐겁게 습득할 수</p>
-        <p>있도록 합니다.</p>
+        <p>균형적 언어접근법 (Balanced Language Approach)을 기반으로 듣고, 말하고, 읽고, 쓰기를 분리하지 않고 이야기와 놀이 그리고 다양한 활동으로 의사소통능력을 골고루 성장시키며 영어에 대한 자신감, 영어에 대한 호기심과 욕구를 충족시키고 성취감을 느끼도록 하며 영어를 즐겁게 습득할 수 있도록 합니다.</p>
       </div>
     </>
   );
@@ -147,35 +158,27 @@ function ArticleThird() {
 function ArticleFourth() {
   return (
     <>
+    <img id="fourth-img-id" className="fourth-right" src="./section/educationalCourse.jpg" alt="아이들이엔씨" />
       <h1 id="fourth-h1-id" className="">Educational course</h1>
       <div id="fourth-div-id" className="fourth-left">
         <h2>영아 / 유아교육 과정</h2>
-        <p>그림책과 다양한 교구, 노래와 챈트 율동, 게임 등으로</p>
-        <p>영어를 처음 접하는 아이들을 위한 놀이수업</p>
-        <p>흥미유발과 호기심자극을 통해 재미있고</p>
-        <p>다양한 영어놀이 경험을 위한 학습</p>
+        <p>그림책과 다양한 교구, 노래와 챈트 율동, 게임 등으로 영어를 처음 접하는 아이들을 위한 놀이수업</p>
+        <p>흥미유발과 호기심자극을 통해 재미있고 다양한 영어놀이 경험을 위한 학습</p>
         <br />
         <h2>유아 / 초등교육 과정</h2>
-        <p>듣고, 말하고, 읽고 쓰는 기초적인 과정을 거쳐</p>
-        <p>주니어토플과 디베이트를 목적으로 자신의</p>
-        <p>생각을 영어로 표현하고 영어에 대한</p>
-        <p>종합적인 지식발달을 위한 학습</p>
+        <p>듣고, 말하고, 읽고 쓰는 기초적인 과정을 거쳐 주니어토플과 디베이트를 목적으로 자신의 생각을 영어로 표현하고 영어에 대한 종합적인 지식발달을 위한 학습</p>
         <br />
         <h2>기관</h2>
-        <p>대집단 수업, 그룹과정으로</p>
-        <p>유치원, 어린이집, 체육센터, 도서관,</p>
-        <p>방과후과정, 문화센터에서</p>
-        <p>진행하는 영어 활동</p>
+        <p>대집단 수업, 그룹과정으로 유치원, 어린이집, 체육센터, 도서관, 방과후과정, 문화센터에서 진행하는 영어 활동</p>
       </div>
-      <img id="fourth-img-id" className="fourth-right" src="./section/educationalCourse.jpg" alt="아이들이엔씨"/>
-    </>
+      </>
   );
 }
 function ArticleFifth() {
   return (
     <>
+    <img id="fifth-img-id" className="fifth-left" src="./section/requestForConsultation.jpg" alt="아이들이엔씨" />
       <h1 id="fifth-h1-id" className="">상담 신청</h1>
-      <img id="fifth-img-id" className="fifth-left" src="./section/requestForConsultation.jpg" alt="아이들이엔씨"/>
       <form id="fifth-form-id" className="fifth-right">
         <h2 className="MapoPeacefull">IDL EDUCATION &amp; CULTURE</h2>
         <p>신청자명(기관)</p>
@@ -192,7 +195,7 @@ function ArticleFifth() {
         <div className="privacy-statement">
           <div id="privacyHtml"></div>
         </div>
-        <div className="privacy-statemen-checkobx">
+        <div className="privacy-statement-checkbox">
           <input type="checkbox" />
           <p>개인정보 수집 및 이용에 동의합니다</p>
         </div>
